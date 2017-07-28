2892316354381cd1d528a9a9a6ecbe5cf3143e54
stuartkilgore <stuart.kilgore@gmail.com>
2017-07-28 16:19:25 -0500

    select styling

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-'use strict';
-window.onload = () => {
-    document.querySelector('#search-bar').addEventListener("Keydown", e => {
-        if e.keyCode === 13 {console.log('Tastycakes')}
-    })
-}
+const DDG = require('node-ddg-api');
+const ddg = new DDG('aca-search');
+
+console.log(ddg);
+
+$(window).ready(() => {
+    const $search = $('#search');
+    const handleSearch = () => {
+        let searchType = $('#select option:selected').val();
+        let query = $search.val();
+        query += "+" + searchType;
+        // CAPTURE QUERY HERE
+        let urlQuery = query.split(' ').join('+');
+        let ddgQuery = `https://duckduckgo.com/?q=${urlQuery}`;
+        window.open(ddgQuery);
+    }
+    $search.keypress(e => {
+        if (e.keyCode === 13) {handleSearch()}         
+    });
+});
