a88e3b4e8ba6483c7e3e0297bde2ef627dae9235
stuartkilgore <stuart.kilgore@gmail.com>
2017-09-03 08:21:09 -0500

    housekeeping

--- script.js
+++ script.js
@@ -7,46 +7,43 @@ const $gitSearch = $('#git-search');
 const $queryBtn = $('#query-btn');
 let query;
 let searchType;
+let gitQuery;
 
 function debounce(func, wait, immediate) {
   let timeout;
   return function () {
-      let context = this,
-          args = arguments;
-      let later = function () {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-      };
-      let callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+    let context = this,
+    args = arguments;
+    let later = () => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
 };
 
 function gSearch() {
-  console.log('gSearching...');
-  let $gList = $('#git-list').children();
-  console.log('$gList: ', $gList);
-  $gList.each(fourth => console.log(fourth));
-//  let $filterList = $gList.each(tr => {
-//    if (TR_TEXT_HERE.indexOf("SEARCH_FOR") > -1) return true;
-//  });
+  console.log('gSearching for ' + gitQuery);
+  let $tr = $('#git-list').children();
+  console.log($td); // <--- LEFT OFF HERE
 }
 
 function capture(query, type) {
   fetch('https://test.campus.austincodingacademy.com/api/queries/56f368f58085ad1100f2ad77', {
-    body: JSON.stringify({data: {query: query, searchType: type.slice(1)}}), 
     method: 'POST', 
-    headers: {'Content-Type': 'application/json'}
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({data: {query: query, searchType: type.slice(1)}})
   })
-  .then(res => { return res.json() })
-  .then(json => {console.log(json)});
+  .then(res => res.json())
+  .then(json => console.log(json));
 }
 
 function getData() {
   fetch('https://test.campus.austincodingacademy.com/api/queries/56f368f58085ad1100f2ad77')
-  .then(res => { return res.json() })
+  .then(res => res.json())
   .then(json => {console.log(json)});
   console.log('got data');
 }
@@ -64,40 +61,56 @@ function handleSearch() {
 const debSearch = debounce(() => {
   query = $search.val();
   console.log(query);
-  fetchTest();
+  instantAns();
 }, 600);
 
-function fetchTest() {
+
+function instantAns() {
   fetch(`https://api.duckduckgo.com/?q=${query.split(' ').join('+')}&format=json&pretty=1`)
   .then(response => {
-      console.log(response);
-      return response.text();
+    console.log(response);
+    return response.text();
   })
   .then(json => {
-      console.log(json);
-      $answer.text(json);
+    console.log(json);
+    $answer.text(json);
   })
   .catch(err => console.log("err:", err));
 }
 
 $(window).ready(() => {
   $search.keypress(e => {
-      query = $search.val();
-      searchType = $('#select option:selected').val();
-      if (e.keyCode === 13) {
-          handleSearch();
-      }
-      debSearch();
+    query = $search.val();
+    searchType = $('#select option:selected').val();
+    if (e.keyCode === 13) {
+        handleSearch();
+    }
+    debSearch();
   });
+
   $searchBtn.click(() => {
-      handleSearch();
+    handleSearch();
   });
+
   $queryBtn.click(() => {
     console.log('queryclick');
     getData();
   });
+
+  $('#git-button').click(() => {
+    $('#git-list').children().hide();
+  });
+
+  $('#git-search').keyup(e => {
+    gitQuery = $('#git-search').val();
+    if (e.target.value === "") {
+      $('#git-list').children().hide();
+      $('.default-git').show();
+    }
+    gSearch();
+  });
+
   $('#git-button').click(() => {
     gSearch();
   });
-  gSearch();
 });
